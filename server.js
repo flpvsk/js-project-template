c450de1fc0a1caf275f3a59c0a8b611aa2f5ce65
filipovskii_off <filipovskii.off@gmail.com>
2012-04-08 01:39:23 +0400

    hogan

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 var requirejs = require('requirejs');
 var connect = require('connect');
+var hoganBuild = require('./hoganbuild');
 
 function rebuildProject() {
+  // r.js build
   var config = JSON.parse(fs.readFileSync('build.js', 'utf8'));
   requirejs.optimize(config, function buildResponse(buildResponse) {
     //buildResponse is just a text output of the modules
@@ -10,6 +12,8 @@ function rebuildProject() {
     //Use config.out to get the optimized file contents.
     console.log(buildResponse);
   });
+
+  hoganBuild.run();
 }
 
 var timeout;
@@ -26,3 +30,5 @@ rebuildProject();
 var server = connect()
   .use(connect.static('public'))
   .listen(8000);
+
+console.log('Listening on 8000...');
